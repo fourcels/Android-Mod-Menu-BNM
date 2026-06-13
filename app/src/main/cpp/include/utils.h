f5f4d2778d5ad0e4657af7107c8bfefb28a1faf3
Kiyan <fourcels@gmail.com>
2026-06-13 11:27:13 +0800

    utils

--- app/src/main/cpp/include/utils.h
+++ app/src/main/cpp/include/utils.h
@@ -12,6 +12,7 @@
 #include <BNM/Method.hpp>
 #include <BNM/Field.hpp>
 #include <BNM/Property.hpp>
+#include <functional>
 
 template<std::size_t N>
 jobjectArray toJobjectArray(JNIEnv *env, std::string (&feats)[N]) {
@@ -90,15 +91,38 @@ T GetField(BNM::IL2CPP::Il2CppObject *instant, const std::string_view &name) {
 }
 
 template<typename T>
+T GetField(void *instant, const std::string_view &name) {
+    return GetField<T>(static_cast<BNM::IL2CPP::Il2CppObject *>(instant), name);
+}
+
+template<typename T>
 T GetProperty(BNM::IL2CPP::Il2CppObject *instant, const std::string_view &name) {
     BNM::Property<T> property = BNM::Class(instant->klass).GetProperty(name);
     return property[instant]();
 }
 
 template<typename T>
+T GetProperty(void *instant, const std::string_view &name) {
+    return GetProperty<T>(static_cast<BNM::IL2CPP::Il2CppObject *>(instant), name);
+}
+
+template<typename T>
 void SetProperty(BNM::IL2CPP::Il2CppObject *instant, const std::string_view &name, T value) {
-    BNM::Property<T> property = BNM::Class(instant->klass).GetProperty(name);
-    property[instant]() = value;
+    BNM::Property<T> property = BNM::Class(instant->klass).GetProperty(name)[instant];
+    property = value;
+}
+
+template<typename T>
+void SetProperty(void *instant, const std::string_view &name, T value) {
+    SetProperty<T>(static_cast<BNM::IL2CPP::Il2CppObject *>(instant), name, value);
+}
+
+template<typename T>
+void ForEachList(void *list, std::function<void(T)> fn) {
+    auto count = GetMethod<int>(list, "get_Count")();
+    for (int i = 0; i < count; ++i) {
+        fn(GetMethod<T>(list, "get_Item")(i));
+    }
 }
 
 #endif //ANDROID_MOD_MENU_BNM_UTILS_H

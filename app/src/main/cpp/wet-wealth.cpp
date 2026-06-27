8899a8e3c56890ffec650927dd03463dbfa0eb46
Kiyan <fourcels@gmail.com>
2026-06-27 23:44:35 +0800

    wet-wealth

--- app/src/main/cpp/wet-wealth.cpp
+++ app/src/main/cpp/wet-wealth.cpp
@@ -10,6 +10,7 @@
 #include <BNM/Method.hpp>
 #include <BNM/Loading.hpp>
 #include <BNM/Property.hpp>
+#include <algorithm>
 #include "utils.h"
 
 
@@ -71,7 +72,12 @@ Java_com_android_support_Menu_valueChange(
 void (*old_AddItem)(void *instance, void *item, int count);
 
 void new_AddItem(void *instance, void *item, int count) {
-    return old_AddItem(instance, item, count * feature.reward);
+    std::vector<int> ids = {550, 551, 1270, 1253, 10267, 10432, 12131, 12307, 14633, 16708};
+    auto unnyId = GetProperty<int>(item, "IntUnnyId");
+    if (std::ranges::contains(ids, unnyId)) {
+        count *= feature.reward;
+    }
+    old_AddItem(instance, item, count);
 }
 
 void (*old_AddCharacterTemptation)(void *instance, void *character, int added);
@@ -85,30 +91,40 @@ void (*old_Load)(void *instance);
 void new_Load(BNM::IL2CPP::Il2CppObject *instance) {
     old_Load(instance);
     if (feature.characters) {
-        auto characters = GetMethod<BNM::IL2CPP::Il2CppObject *>(instance, "get_Characters")();
+        auto userCharacterFactory = GetField<void *>(instance, "userCharacterFactory");
+        auto characterStaticUsecase = GetField<void *>(userCharacterFactory,
+                                                       "characterStaticUsecase");
+        auto characters = GetProperty<void *>(characterStaticUsecase,
+                                              "Characters");
         auto count = GetMethod<int>(characters, "get_Count")();
         for (int i = 0; i < count; i++) {
-            auto character = GetMethod<BNM::IL2CPP::Il2CppObject *>(characters, "get_Item")(i);
-            GetMethod<void>(instance, "AddCharacterExp")(character, 100);
+            auto character = GetMethod<void *>(characters, "get_Item")(i);
+            auto id = GetProperty<int>(character, "Id");
+            GetMethod<void>(instance, "AddCharacterOrExperience")(id, 100);
         }
     }
 }
 
+bool (*old_TryBuyVip)(void *instance);
 
-int (*old_GetLocationCurrentLevel)(void *instance, void *config);
+bool new_TryBuyVip(void *instance) {
+    auto monthlyVipUsecase = GetField<void *>(instance, "monthlyVipUsecase");
+    auto userInventoryUsecase = GetField<void *>(monthlyVipUsecase, "userInventoryUsecase");
+    auto vipAccessKey = GetMethod<void *>(instance, "GetVipAccessKey")();
+    GetMethod<void>(userInventoryUsecase, "AddItem")(vipAccessKey, 1);
+    return true;
+}
 
-int new_GetLocationCurrentLevel(BNM::IL2CPP::Il2CppObject *instance,
-                                BNM::IL2CPP::Il2CppObject *config) {
-    int level = old_GetLocationCurrentLevel(instance, config);
-    if (level > 5) {
-        auto staticItems = GetProperty<BNM::IL2CPP::Il2CppObject *>(config, "LocationKey");
-        auto userInventoryUsecase = GetField<BNM::IL2CPP::Il2CppObject *>(instance,
-                                                                          "userInventoryUsecase");
-        auto RemoveItem = GetMethod<void>(userInventoryUsecase, "RemoveItem");
-        RemoveItem(staticItems, level - 5);
-        level = 5;
-    }
-    return level;
+void (*old_TryBuy)(void *instance, void *config);
+
+void new_TryBuy(void *instance, void *config) {
+    auto key = GetProperty<void *>(config, "Key");
+    auto usecase = GetField<void *>(instance, "usecase");
+    auto inventoryUsecase = GetField<void *>(usecase, "inventoryUsecase");
+    GetMethod<void>(inventoryUsecase, "AddItem")(key, 1);
+
+    auto OnPurchased = GetField<void *>(instance, "OnPurchased");
+    GetMethod<void>(OnPurchased, "Invoke")(config);
 }
 
 
@@ -126,15 +142,19 @@ void OnLoaded() {
     auto Load = UserCharactersUsecase.GetMethod("Load");
     auto AddCharacterTemptation = UserCharactersUsecase.GetMethod("AddCharacterTemptation");
 
-    auto CityPaidLocationsUsecase = BNM::Class("WetWealth.PaidLocations",
-                                               "CityPaidLocationsUsecase",
-                                               AssemblyCSharp);
-    auto GetLocationCurrentLevel = CityPaidLocationsUsecase.GetMethod("GetLocationCurrentLevel");
+    auto MonthlyVipPresenter = BNM::Class("WetWealth.SlotMachine",
+                                          "MonthlyVipPresenter",
+                                          AssemblyCSharp);
+    auto TryBuyVip = MonthlyVipPresenter.GetMethod("TryBuyVip");
 
+    auto BuildingRepairPresenter = BNM::Class("WetWealth.BuildingRepair",
+                                              "BuildingRepairPresenter",
+                                              AssemblyCSharp);
+    auto TryBuy = BuildingRepairPresenter.GetMethod("TryBuy");
 
     BNM::BasicHook(AddItem, new_AddItem, old_AddItem);
     BNM::BasicHook(Load, new_Load, old_Load);
     BNM::BasicHook(AddCharacterTemptation, new_AddCharacterTemptation, old_AddCharacterTemptation);
-    BNM::BasicHook(GetLocationCurrentLevel, new_GetLocationCurrentLevel,
-                   old_GetLocationCurrentLevel);
+    BNM::BasicHook(TryBuyVip, new_TryBuyVip, old_TryBuyVip);
+    BNM::BasicHook(TryBuy, new_TryBuy, old_TryBuy);
 }
